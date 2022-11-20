48edbbbfce024211070fe5100689b3e6d6859773
paloma Schultze <palomaschultze@gmail.com>
2022-11-19 23:56:01 -0300

    v6 form footer y js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// js
+
+
+
+// jQuery
+$(document).ready(function(){
+
+    $("#destacado1").click(function(){
+        $("#contenidoreceta1").toggle();
+        $("#contenidoreceta2").toggle();
+        $("#contenidoreceta3").toggle();
+      });
+
+      $("#destacado2").click(function(){
+        $("#contenidodestacado1").toggle();
+        $("#contenidodestacado2").toggle();
+        $("#contenidodestacado3").toggle();
+        $("#contenidodestacado4").toggle();
+      });
+
+      $("#destacado3").click(function(){
+        $("#contenidodestacado1").toggle();
+        $("#contenidodestacado2").toggle();
+        $("#contenidodestacado3").toggle();
+        $("#contenidodestacado4").toggle();
+      });
+
+      $("#destacado4").click(function(){
+        $("#contenidodestacado1").toggle();
+        $("#contenidodestacado2").toggle();
+        $("#contenidodestacado3").toggle();
+        $("#contenidodestacado4").toggle();
+      });
+
+})
